d9d4d2348345893fae311ccd9264df25e6bfb1b7
Andra <andrapop291@gmail.com>
2018-01-09 20:33:20 +0200

    Removed old unfinished LR1

--- LFTC/LR1/src/Main.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by andrapop on 2017-12-24.
- */
-public class Main {
-    public static void main(String[] args) {
-        Parser parser = new Parser("res/grammar.txt");
-        parser.generateParsingAutomatum();
-    }
-}
